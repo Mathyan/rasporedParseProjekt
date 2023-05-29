f155bc887ef68497082550bc279231f6c4184435
Matijan Cvitanovic <matijan.cvitanovic@outlook.com>
2023-05-30 00:46:13 +0200

    Week management and history

--- RasporedClass.py
+++ RasporedClass.py
@@ -1,20 +1,38 @@
-
 class Raspored:
-    __prezime_ime = ""
-    __dani = ["Ponedjeljak:", "Utorak:", "Srijeda:", "Cetvrtak:", "Petak:", "Subota:", "Nedjelja:"]
-    __sati_po_danima = {}
+    day_names = ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja']
+    def __init__(self):
+        self.weeks = {}
 
-    for item in __dani:
-        key = item[:-1]  # Remove the colon at the end of each item
-        __sati_po_danima[key] = ""
+    def add_week_data(self, week_number, name, schedule_data):
+        if week_number not in self.weeks:
+            self.weeks[week_number] = {}
+        if name not in self.weeks[week_number]:
+            self.weeks[week_number][name] = []
+        day_schedule = {day: schedule for day, schedule in zip(self.day_names, schedule_data)}
+        self.weeks[week_number][name].append(day_schedule)
 
-    def __init__(self, prezime_ime, lista_sati: list):
-        self.__prezime_ime = prezime_ime
-        for i in range(len(lista_sati)):
-            self.__sati_po_danima[self.__dani[i]] = lista_sati[i]
+    def get_last_schedule_version(self, week_number, name):
+        if week_number in self.weeks and name in self.weeks[week_number]:
+            schedules = self.weeks[week_number][name]
+            if schedules:
+                return schedules[-1]
+        return None
 
-    def get_prezime_ime(self):
-        return self.__prezime_ime
+    def get_all_last_versions(self):
+        last_versions = {}
+        for week_number, week_data in self.weeks.items():
+            for name, schedules in week_data.items():
+                if schedules:
+                    last_versions[name] = schedules[-1]
+        return last_versions
 
-    def get_raspored(self):
-        return self.__sati_po_danima
+    def get_week_data(self, week_number):
+        return self.weeks.get(week_number, {})
+
+    def get_versions_count(self, week_number, name):
+        if week_number in self.weeks and name in self.weeks[week_number]:
+            return len(self.weeks[week_number][name])
+        return 0
+
+    def get_weeks(self):
+        return self.weeks
--- rasporedParse.py
+++ rasporedParse.py
@@ -6,7 +6,6 @@ import time
 import os
 import re
 from RasporedClass import Raspored
-
 def login(username, password):
     options = Options()
     options.add_argument('-headless')
@@ -45,20 +44,21 @@ def save_page(driver):
         f.write(driver.page_source)
 
 def get_raspored(driver):
-    raspored_html = driver.find_element(By.ID, '1S').get_attribute('innerHTML')
+    raspored_html = driver.find_element(By.ID, 'uniRaspored').get_attribute('innerHTML')
     return extract_raspored(raspored_html)
 
 def extract_raspored(raspored_html):
     # Extract the schedule
     time_cells = re.findall(r'<span class="dan_razdoblje".*?>(.*?)</span><span class="dan_ukupno".*?>(.*?)</span>', raspored_html)
     names = re.findall(r'<div class="tjedan_djelatnik" style="[^"]*" title="([^"]+)"[^>]*>[^<]*<\/div>', raspored_html)
+    week_number = re.findall(r'Tjedan (\d+)', raspored_html)
     # Clean up the extracted data
     while ('Razdoblje', 'Sati') in time_cells:
         time_cells.remove(('Razdoblje', 'Sati'))
     for i in range(len(time_cells)):
         if time_cells[i] == ('&nbsp;', '&nbsp;'):
             time_cells[i] = ('none', 'none')
-    return names[0] , time_cells
+    return int(week_number[0]) ,names[0] , time_cells
 def write_csv(raspored):
     # Write the schedule to a CSV file
     pass
@@ -75,7 +75,10 @@ def main():
 
     save_page(driver)
     write_csv(get_raspored(driver))
-    raspored = Raspored(get_raspored(driver))
+    week, name, raspored_data = get_raspored(driver)
+    raspored = Raspored()
+    raspored.add_week_data(week , name, raspored_data)
+    print(raspored.get_weeks())
     # Quit the driver
     driver.quit()
 
